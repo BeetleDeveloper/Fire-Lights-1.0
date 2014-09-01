27bdb7d425083312c04139e697edbdf8a25e7e07
JuanDavid <jdlandazaball@gmail.com>
2014-09-01 09:48:46 -0430

    First Ascent of 1.0
    Rom 1.0 Basada en FFOX 1.4

---
+++ system/b2g/defaults/pref/user.js
@@ -0,0 +1,18 @@
+pref('browser.manifestURL', "app://system.gaiamobile.org/manifest.webapp");
+pref('b2g.neterror.url', "app://system.gaiamobile.org/net_error.html");
+pref('browser.homescreenURL', "app://system.gaiamobile.org/index.html");
+pref('network.http.max-connections-per-server', 15);
+pref('dom.mozInputMethod.enabled', true);
+pref('layout.css.sticky.enabled', true);
+pref('ril.debugging.enabled', false);
+pref('dom.mms.version', 17);
+pref('b2g.wifi.allow_unsafe_wpa_eap', true);
+pref('network.dns.localDomains', "gaiamobile.org,system.gaiamobile.org,sms.gaiamobile.org,video.gaiamobile.org,homescreen.gaiamobile.org,bluetooth.gaiamobile.org,search.gaiamobile.org,costcontrol.gaiamobile.org,pdfjs.gaiamobile.org,ringtones.gaiamobile.org,calendar.gaiamobile.org,wappush.gaiamobile.org,email.gaiamobile.org,fm.gaiamobile.org,settings.gaiamobile.org,gallery.gaiamobile.org,keyboard.gaiamobile.org,camera.gaiamobile.org,communications.gaiamobile.org,setringtone.gaiamobile.org,fl.gaiamobile.org,music.gaiamobile.org,clock.gaiamobile.org,wallpaper.gaiamobile.org,browser.gaiamobile.org,marketplace.firefox.com.gaiamobile.org");
+pref("geo.gps.supl_server", "supl.izatcloud.net");
+pref("geo.gps.supl_port", 22024);
+pref("dom.payment.provider.0.name", "firefoxmarket");
+pref("dom.payment.provider.0.description", "marketplace.firefox.com");
+pref("dom.payment.provider.0.uri", "https://marketplace.firefox.com/mozpay/?req=");
+pref("dom.payment.provider.0.type", "mozilla/payments/pay/v1");
+pref("dom.payment.provider.0.requestMethod", "GET");
+
